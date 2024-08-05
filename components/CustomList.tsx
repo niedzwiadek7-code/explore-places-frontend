ebe7f4114143adf61f527dc708ded4d8d872626a
Damian Kliber <damiankliber@gmail.com>
2024-08-05 20:30:17 +0200

    feat: improve custom list

--- components/CustomList.tsx
+++ components/CustomList.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement, useState } from 'react'
-import { Dimensions, FlatList } from 'react-native'
+import React, { ReactElement, useRef, useState } from 'react'
+import { FlatList } from 'react-native'
 
 type IBasicObj = {
   id: string | number
@@ -13,29 +13,7 @@ type IProps<T extends IBasicObj> = {
 
 const CustomList = <T extends IBasicObj>({ data, fetchMoreData, renderItem }: IProps<T>) => {
   const [localData, setLocalData] = useState<T[]>(data)
-  const listRef = React.useRef<FlatList>(null)
-  const [startScrollY, setStartScrollY] = useState<number>(0)
-  const [actualIndex, setActualIndex] = useState<number>(0)
-  const windowHeight = Dimensions.get('window').height
-
-  const scrollToIndex = async (index: number) => {
-    if (index >= 0 && index < localData.length) {
-      if (listRef.current) {
-        listRef.current.scrollToIndex({
-          index,
-          animated: true,
-        })
-      }
-      setActualIndex(index)
-    }
-
-    if (index > localData.length - 2) {
-      if (fetchMoreData) {
-        const fetchedData = await fetchMoreData()
-        setLocalData([...localData, ...fetchedData])
-      }
-    }
-  }
+  const listRef = useRef<FlatList>(null)
 
   return (
     <FlatList
@@ -53,23 +31,14 @@ const CustomList = <T extends IBasicObj>({ data, fetchMoreData, renderItem }: IP
       keyExtractor={(item) => item.id}
       showsHorizontalScrollIndicator={false}
       showsVerticalScrollIndicator={false}
-      onScrollBeginDrag={(event) => {
-        const { y } = event.nativeEvent.contentOffset
-        setStartScrollY(y)
-      }}
-      onScrollEndDrag={async (event) => {
-        const { y } = event.nativeEvent.contentOffset
-        const heightToScroll = windowHeight * 0.15
-        const scrolled = y - startScrollY
-
-        if (scrolled > heightToScroll) {
-          await scrollToIndex(actualIndex + 1)
-        } else if (scrolled < -heightToScroll) {
-          await scrollToIndex(actualIndex - 1)
-        } else {
-          await scrollToIndex(actualIndex)
+      onViewableItemsChanged={async ({ viewableItems }) => {
+        const actualIndex = viewableItems[0].index
+        if (actualIndex && actualIndex > localData.length - 2 && fetchMoreData) {
+          const newData = await fetchMoreData()
+          setLocalData([...localData, ...newData])
         }
       }}
+      pagingEnabled
     />
   )
 }
